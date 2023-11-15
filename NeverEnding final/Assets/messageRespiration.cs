c654cb8d5d7dc57e9be0b360e6420a7a6fdf7bdc
Irene Bergandi <irene.bergandi@mail.polimi.it>
2023-11-15 19:02:46 +0100

    sfera che si allarga, messaggio di respirare

---
+++ NeverEnding final/Assets/messageRespiration.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class messageRespiration : MonoBehaviour
+{
+    public Text textField;
+   
+    void Start()
+    {
+        textField.text = "Respira a ritmo con la sfera";
+    }
+    
+}
+
+
